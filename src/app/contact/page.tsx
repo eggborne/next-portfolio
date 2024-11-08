16ae06cff5b9fd71b66deb7451288249bc1e0e8a
eggborne <mike@mikedonovan.dev>
2024-11-08 16:02:21 -0700

    Fix contact formatting, adjust style

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,15 +7,21 @@ export const metadata = {
 };
 
 export default function Contact() {
+  const formattedPhone = contactData.phone.replace(/\+1-(\d{3})-(\d{3})-(\d{4})/, "($1)-$2-$3");
   return (
     <section className={styles.Contact}>
       <div className={styles.contactInfo}>
         <h2>Contact Information</h2>
-        <p><strong>Email:</strong> <a href={`mailto:mike@${contactData.email}`}>{contactData.email}</a></p>
-        <p><strong>Phone:</strong><a href={`tel:${contactData.phone}`}>{contactData.phone}</a></p>
-        <p><strong>Location:</strong> {contactData.location.city}, {contactData.location.state}, {contactData.location.country}</p>
-        <p><strong>LinkedIn:</strong> <a href={contactData.socials.linkedin} target="_blank" rel="noopener noreferrer">{contactData.socials.linkedin.split('/').pop()}</a></p>
-        <p><strong>GitHub:</strong> <a href={contactData.socials.github} target="_blank" rel="noopener noreferrer">{contactData.socials.github.split('/').pop()}</a></p>
+        <strong>Email:</strong>
+        <p><a href={`mailto:${contactData.email}`}>{contactData.email}</a></p>
+        <strong>Phone:</strong>
+        <p><a href={`tel:${contactData.phone}`}>{formattedPhone}</a></p>
+        <strong>Location:</strong>
+        <p>{contactData.location.city}, {contactData.location.state}, {contactData.location.country}</p>
+        <strong>LinkedIn:</strong>
+        <p><a href={contactData.socials.linkedin} target="_blank" rel="noopener noreferrer">{contactData.socials.linkedin.split('/').pop()}</a></p>
+        <strong>GitHub:</strong>
+        <p><a href={contactData.socials.github} target="_blank" rel="noopener noreferrer">{contactData.socials.github.split('/').pop()}</a></p>
       </div>
       <ContactForm />
     </section>
